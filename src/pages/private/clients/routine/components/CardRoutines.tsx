a286cb0b6a2b51a9ae5acd2c95393c4622bd045a
Joaquin <joaqobianchi13@gmail.com>
2024-12-05 12:35:26 -0300

    Remove print clases

--- src/pages/private/clients/routine/components/CardRoutines.tsx
+++ src/pages/private/clients/routine/components/CardRoutines.tsx
@@ -19,14 +19,11 @@ export default function CardRoutines({ routine, exercises }: Props) {
   };
 
   return (
-    <Card
-      key={routine.id}
-      className="shadow-lg print:shadow-none print:border print:border-gray-300"
-    >
-      <CardHeader className="print:bg-white print:text-black">
+    <Card key={routine.id} className="shadow-lg">
+      <CardHeader className="">
         <CardTitle className="flex justify-between items-center">
           <div className="capitalize flex items-baseline">
-            <Calendar className="mr-2 h-4 w-4 print:text-black" />
+            <Calendar className="mr-2 h-4 w-4 " />
             <span className="capitalize">
               {routine.day === "miercoles"
                 ? "Miércoles"
@@ -40,11 +37,11 @@ export default function CardRoutines({ routine, exercises }: Props) {
               variant="outline"
               size="icon"
               onClick={handlePrint}
-              className="print:hidden"
+              className=""
             >
               <Printer className="h-4 w-4" />
             </Button>
-            <div className="print:hidden">
+            <div className="">
               <ActionModalUserRutine dialogTitle="Asignar Rutina">
                 {(closeModal) => (
                   <AddExerciseForm
@@ -58,13 +55,13 @@ export default function CardRoutines({ routine, exercises }: Props) {
           </div>
         </CardTitle>
       </CardHeader>
-      <CardContent className="print:bg-white print:text-black">
+      <CardContent className="">
         {routine.routineExercises?.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 print:gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
             {routine.routineExercises.map((exercise) => (
               <div
                 key={exercise.id}
-                className="bg-secondary/10 rounded-lg p-4 shadow-sm print:bg-white print:border print:border-gray-300 print:break-inside-avoid"
+                className="bg-secondary/10 rounded-lg p-4 shadow-sm "
               >
                 <div className="flex flex-col sm:flex-row gap-4 h-full">
                   <div className="sm:w-[100px] aspect-video sm:aspect-square">
@@ -74,7 +71,7 @@ export default function CardRoutines({ routine, exercises }: Props) {
                         "https://app-media.fitbod.me/v2/102/images/landscape/0_960x540.jpg"
                       }
                       alt={exercise.exercise.name}
-                      className="rounded-md w-full h-full object-cover object-center print:filter print:grayscale"
+                      className="rounded-md w-full h-full object-cover object-center "
                     />
                   </div>
 
@@ -85,48 +82,31 @@ export default function CardRoutines({ routine, exercises }: Props) {
                       </span>
                       <div className="flex flex-wrap items-center gap-2 mb-4">
                         {exercise.sets !== null ? (
-                          <Badge className="print:bg-white print:text-black print:border print:border-gray-300">
-                            {exercise.sets} Series
-                          </Badge>
+                          <Badge className="  ">{exercise.sets} Series</Badge>
                         ) : (
-                          <Badge
-                            variant="outline"
-                            className="print:bg-white print:text-black print:border print:border-gray-300"
-                          >
+                          <Badge variant="outline" className="  ">
                             <s>Series</s>
                           </Badge>
                         )}
                         {exercise.reps !== null ? (
-                          <Badge className="print:bg-white print:text-black print:border print:border-gray-300">
-                            {exercise.reps} Reps
-                          </Badge>
+                          <Badge className="  ">{exercise.reps} Reps</Badge>
                         ) : (
-                          <Badge
-                            variant="outline"
-                            className="print:bg-white print:text-black print:border print:border-gray-300"
-                          >
+                          <Badge variant="outline" className="  ">
                             <s>Reps</s>
                           </Badge>
                         )}
                         {exercise.time !== null ? (
-                          <Badge className="print:bg-white print:text-black print:border print:border-gray-300">
-                            {exercise.time} Minutos
-                          </Badge>
+                          <Badge className="  ">{exercise.time} Minutos</Badge>
                         ) : (
-                          <Badge
-                            variant="outline"
-                            className="print:bg-white print:text-black print:border print:border-gray-300"
-                          >
+                          <Badge variant="outline" className="  ">
                             <s>Minutos</s>
                           </Badge>
                         )}
                       </div>
                       <div>
                         {exercise.comment !== null && (
-                          <p className="text-xs text-gray-200 font-medium print:text-gray-600">
-                            <span className="text-primary print:text-black">
-                              -
-                            </span>{" "}
+                          <p className="text-xs text-gray-200 font-medium ">
+                            <span className="text-primary ">-</span>{" "}
                             {exercise.comment}
                           </p>
                         )}
@@ -134,7 +114,7 @@ export default function CardRoutines({ routine, exercises }: Props) {
                     </div>
                   </div>
 
-                  <div className="self-end print:hidden">
+                  <div className="self-end ">
                     <ButtonIconDelete
                       id={exercise.id}
                       deleteFn={deleteRoutinExercise}
@@ -148,7 +128,7 @@ export default function CardRoutines({ routine, exercises }: Props) {
             ))}
           </div>
         ) : (
-          <p className="text-muted-foreground print:text-gray-600">
+          <p className="text-muted-foreground">
             No hay rutina asignada para este día.
           </p>
         )}
